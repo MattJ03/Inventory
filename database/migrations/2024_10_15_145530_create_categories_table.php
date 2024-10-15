d73a834e4a127ac1703d17010fb11aa421256569
Matt Shaw <Shaw-M18@ulster.ac.uk>
2024-10-15 21:09:46 +0100

    text rather than string, simple change as string wont work for a longer description

--- database/migrations/2024_10_15_145530_create_categories_table.php
+++ database/migrations/2024_10_15_145530_create_categories_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->string('description')->nullable();
+            $table->text('description')->nullable();
+
             $table->timestamps();
         });
     }
