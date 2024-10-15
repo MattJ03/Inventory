7e1f802c4c2a47bead77721b111d0f5bfe90b72f
Matt Shaw <Shaw-M18@ulster.ac.uk>
2024-10-15 21:01:41 +0100

    updated Product model

--- database/migrations/2024_10_15_145452_create_products_table.php
+++ database/migrations/2024_10_15_145452_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('quantity');
+            $table->decimal('price', 8, 2);
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('supplier_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
