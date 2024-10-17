8d7835132c9400a7934f5cd2223ff96660d40a39
Matt Shaw <Shaw-M18@ulster.ac.uk>
2024-10-18 00:00:53 +0100

    updated some changes into the SupplierController mainly changing from returning view and now redirecting as it will stop issue when the user refreshes the page which may double submit the form

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -27,17 +27,17 @@ class SupplierController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Supplier $supplier)
     {
         $validated = $request->validate([
             'name' => 'required|string|max:255',
             'contact_name' => 'required|string|max:255',
-            'email' => 'nullable|unique:suppliers|email|max:255',
-            'phone' => 'nullable|integer|digits_between:10,12',
+            'email' => 'nullable|email|max:255|unique:suppliers|email',
+            'phone' => 'nullable|string|max:255',
             'address' => 'nullable|text|max:255',
         ]);
-        \App\Models\Supplier::create($validated);
-        return view('supplier.index');
+         \App\Models\Supplier::create($validated);
+        return redirect()->route('supplier.index')->with('success Supplier created');
     }
 
     /**
@@ -72,7 +72,7 @@ class SupplierController extends Controller
         ]);
 
         $supplier->update($validated);
-        return view('supplier.show', compact('supplier'));
+        return redirect()->route('supplier.index')->with('success Supplier updated');
 
     }
 
@@ -81,7 +81,8 @@ class SupplierController extends Controller
      */
     public function destroy(string $id)
     {
-        $supplier = \App\Models\Supplier::findOrFail($id);
-        $supplier->delete();
+       $supplier = \App\Models\Supplier::findOrFail($id);
+       $supplier->delete();
+       return redirect()->route('supplier.index')->with('success Supplier deleted');
     }
 }
