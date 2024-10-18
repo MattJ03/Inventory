683ff86c717d78bbbbfb899f3c9ef51f4e45c0d8
Matt Shaw <Shaw-M18@ulster.ac.uk>
2024-10-18 15:50:04 +0100

    completed app.php file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,5 +6,22 @@
     <title>Inventory app</title>
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
+<body class="bg-amber-50">
+<div class="container mx-auto px-4">
+    <header class="py-6">
+        <h1 class="text-2xl font-bold">Inventory Management app</h1>
+        <nav>
+            <a href="{{ route('products.index') }}" class="mr-4">Products</a>
+            <a href="{{ route('categories.index') }}" class="mr-4">Categories</a>
+            <a href="{{ route('suppliers.index') }}" class="mr-4">Suppliers</a>
+        </nav>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+</div>
+</body>
 
 </html>
