bf48b418850ba965a49712717d732c53cbc0e8e4
Matt Shaw <Shaw-M18@ulster.ac.uk>
2024-10-18 15:00:25 +0100

    added breeze to routes for login and authentication

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
@@ -15,29 +16,46 @@ use App\Http\Controllers\SupplierController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-//product routes
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+// Product Routes
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
 Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-Route::get('/products{$id}', [ProductController::class, 'show'])->name('products.show');
-Route::get('/products{$id}/edit', [ProductController::class, 'edit'])->name('products.edit');
-Route::put('/products{$id}', [ProductController::class, 'update'])->name('products.update');
-Route::delete('/products{$id}', [ProductController::class, 'destroy'])->name('products.destroy');
+Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
+Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
+Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
+Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
 
-//Cateogry routes
+// Category Routes
 Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
 Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
 Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
-Route::get('/categories{$id}', [CategoryController::class, 'show'])->name('categories.show');
-Route::get('/categories{$id}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
-Route::put('/categories{$id}', [CategoryController::class, 'update'])->name('categories.update');
-Route::delete('/categories{$id}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+Route::get('/categories/{id}', [CategoryController::class, 'show'])->name('categories.show');
+Route::get('/categories/{id}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+Route::put('/categories/{id}', [CategoryController::class, 'update'])->name('categories.update');
+Route::delete('/categories/{id}', [CategoryController::class, 'destroy'])->name('categories.destroy');
 
-//supplier routes
+// Supplier Routes
 Route::get('/suppliers', [SupplierController::class, 'index'])->name('suppliers.index');
-Route::get('/supplier/create', [SupplierController::class, 'create'])->name('suppliers.create');
+Route::get('/suppliers/create', [SupplierController::class, 'create'])->name('suppliers.create');
 Route::post('/suppliers', [SupplierController::class, 'store'])->name('suppliers.store');
-Route::get('/suppliers{$id}/edit', [SupplierController::class, 'edit'])->name('suppliers.edit');
-Route::put('/suppliers{$id}', [SupplierController::class, 'update'])->name('suppliers.update');
-Route::get('/supplier{$id}', [SupplierController::class, 'show'])->name('suppliers.show');
-Route::delete('suppliers{$id}', [SupplierController::class, 'destroy'])->name('suppliers.destroy');
+Route::get('/suppliers/{id}', [SupplierController::class, 'show'])->name('suppliers.show');
+Route::get('/suppliers/{id}/edit', [SupplierController::class, 'edit'])->name('suppliers.edit');
+Route::put('/suppliers/{id}', [SupplierController::class, 'update'])->name('suppliers.update');
+Route::delete('/suppliers/{id}', [SupplierController::class, 'destroy'])->name('suppliers.destroy');
