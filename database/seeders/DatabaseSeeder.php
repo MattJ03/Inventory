073f7af1d81193dee32b558d34e0468bdb3d7b96
Matt Shaw <Shaw-M18@ulster.ac.uk>
2024-10-21 21:54:47 +0100

    permission for create

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+     $adminRole = Role::create(['name' => 'admin']);
+     $userRole = Role::create(['name' => 'user']);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+     Permission::create(['name' => 'product.index']);
+     Permission::create(['name' => 'product.create']);
+     Permission::create(['name' => 'product.edit']);
+     Permission::create(['name' => 'product.destroy']);
+
     }
 }
